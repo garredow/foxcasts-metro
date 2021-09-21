a8a0455aa457e515ff480ef42f0be37913481d4e
Garrett Downs <garredow@outlook.com>
2021-09-21 12:03:37 -0400

    add appbar

---
+++ src/ui-components/AppBar.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { ComponentBaseProps } from '../models';
+import { ifClass, joinClasses } from '../utils/classes';
+import { Icon } from './Icon';
+import styles from './AppBar.module.css';
+
+export type TopItem = {
+  id: string;
+  icon: string;
+  label: string;
+};
+
+export type BottomItem = {
+  id: string;
+  label: string;
+};
+
+type Props = ComponentBaseProps & {
+  buttons: TopItem[];
+  listItems?: BottomItem[];
+  onAction?: (action: string) => void;
+};
+
+export function AppBar({ ...props }: Props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className={joinClasses(styles.root, ifClass(open, styles.open))}>
+      <div className={styles.buttons}>
+        <div className={styles.placeholder}></div>
+        <div className={styles.flex}></div>
+        {props.buttons.map((a) => (
+          <Icon
+            key={a.id}
+            className={styles.button}
+            icon={a.icon}
+            label={a.label}
+            onClick={() => props.onAction?.(a.id)}
+          />
+        ))}
+        <div className={styles.flex}></div>
+        <Icon icon="overflow-dots" onClick={() => setOpen(!open)} />
+      </div>
+    </div>
+  );
+}
--- src/ui-components/Icon.tsx
+++ src/ui-components/Icon.tsx
@@ -5,16 +5,22 @@ import styles from './Icon.module.css';
 type Props = ComponentBaseProps & {
   icon: string;
   size?: 'small' | 'medium' | 'large';
+  label?: string;
   onClick?: () => void;
 };
 
 export function Icon({ size = 'medium', ...props }: Props) {
   return (
     <div
-      className={joinClasses(styles.root, props.className)}
+      className={joinClasses(styles.root, styles[size], props.className)}
       onClick={props.onClick}
     >
-      <img className={styles[size]} src={`/icons/${props.icon}.png`} alt="" />
+      <img
+        className={styles[`icon-${size}`]}
+        src={`/icons/${props.icon}.png`}
+        alt=""
+      />
+      {props.label ? <div className={styles.label}>{props.label}</div> : null}
     </div>
   );
 }
